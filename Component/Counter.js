8badcd17bee41c0200d36979f528202a9f75478f
dayun <112678593+kdayun@users.noreply.github.com>
2024-02-19 15:22:14 +0900

    Create Counter.js

---
+++ Component/Counter.js
@@ -0,0 +1,30 @@
+import {Component} from "react";
+
+class Counter extends Component {
+   state = {
+            number: 0,
+            fixedNumb: 0
+    };
+
+    render(){
+        const {number, fixedNumb} = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumb}</h2>
+                <button
+                    onClick={()=> {
+                        this.setState(prevState => {
+                            return {number:  prevState.number +1};
+                        })
+                        {/*this.setState({fixedNumb: fixedNumb+10});*/}
+                        this.setState(prevState => {
+                            return {number:  prevState.number +1};
+                        })
+                    }}>+1</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
