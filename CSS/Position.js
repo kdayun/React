6799215a4a7a23bdcd1736575e986b857c24b6ae
dayun <112678593+kdayun@users.noreply.github.com>
2024-02-24 22:15:10 +0900

    Add files via upload

---
+++ CSS/Position.js
@@ -0,0 +1,32 @@
+import "./Position.css";
+
+function Position(){
+    return(
+        <>
+            <div className="a"></div>
+            <div className="b">
+                <div className="abs"></div>
+            </div>    
+            <hr></hr>
+
+            <div>
+                첫번째<br/>첫번째<br/>첫번째<br/>첫번째<br/>
+                첫번째<br/>첫번째<br/>첫번째<br/>첫번째<br/>
+                첫번째<br/>첫번째<br/>첫번째<br/>첫번째<br/>
+                첫번째<br/>첫번째<br/>첫번째<br/>첫번째<br/>
+            </div>
+            <div className="pos"></div>
+            <div>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            두번째<br/>두번째<br/>두번째<br/>두번째<br/>
+            </div>
+        </>
+        
+    )
+}
+
+export default Position;
