85c874f7ef85bfac01a7a04b1d519f17fb0f93d1
dayun <112678593+kdayun@users.noreply.github.com>
2024-03-02 17:04:50 +0900

    Create PseudoClass.js

---
+++ CSS/PseudoClass.js
@@ -0,0 +1,24 @@
+import "./flex.css";
+
+
+function Flex(){
+
+    return(
+        <div>
+            <input type="button" value="버튼"></input>
+            <input type="text" placeholder="아무거나 쓰기" disabled></input>
+            <br></br>
+            <div className="boxes">
+                <div className="box">1번</div>
+                <div className="box">2번</div>
+                <div className="box">3번</div>
+                <div className="box">4번</div>
+                <div className="box">5번</div>
+            </div>
+                
+            
+        </div>
+    );
+}
+
+export default Flex;
